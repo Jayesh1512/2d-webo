eb0d31944c03d9b02e3d538ecdeca3ddb56ff065
Jayesh1512 <131896146+Jayesh1512@users.noreply.github.com>
2024-01-17 02:21:28 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,92 @@
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Blender",
+    Image: "/blender.svg",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Three Js",
+    Image: "/threejs.svg",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.png",
+    url: "https://www.instagram.com/jayeshh.bot/"
+  },
+  {
+    name: "LinkedIn",
+    src: "/linkedin.png",
+    url: "https://www.linkedin.com/in/jayesh-shete-267177257/"
+  },
+  {
+    name: "Github",
+    src: "/github.png",
+    url: "https://github.com/Jayesh1512"
+  },
+];
+
+
+
